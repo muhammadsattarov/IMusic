e33222f8ae1aea5a2e031294b72962e0b1024a5d
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-02-08 17:57:23 +0500

    Initial Commit

---
+++ IMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMusic
+//
+//  Created by user on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
